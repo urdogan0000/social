05382bd6bab00e157d802407c9b7fa6bd366e8dd
haydar.urdogan <hydrurdgn@gmail.com>
2025-11-11 16:23:29 +0300

    first commit

---
+++ cmd/api/api.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type application struct {
+	config config
+}
+
+type config struct {
+	addr string
+}
+
+func (app *application) mount() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /v1/health", app.healthCheckHandler)
+	return mux
+}
+
+func (app *application) run(mux *http.ServeMux) error {
+	srv := http.Server{
+		Addr:         app.config.addr,
+		Handler:      mux,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Minute,
+	}
+
+	log.Printf("server has started at %s", app.config.addr)
+
+	return srv.ListenAndServe()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/urdogan0000/social
+
+go 1.25.4
